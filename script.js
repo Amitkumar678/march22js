da82958d8848888292fbfc446ec6f271981457c7
amitkumar <realamit678@gmail.com>
2022-03-23 13:37:11 +0530

    no commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// var age=29;
+// if(age<30){
+//     console.log('Adult')
+// }
+// else{
+//     console.log('Old')
+// }
+// var age=34;
+// (age<30)?console.log('Adult'):console.log('Old');
+
+
+var day='thursday';
+switch(day){
+case 'monday':{
+    console.log('monday');
+}
+break
+
+case 'tuesday':{
+    console.log('TUESDAY');
+}
+break
+
+case 'wednesday':{
+    console.log('WEDNESDAY');
+}
+break
+
+case 'thursday':{
+    console.log('THURSDAY');
+}
+break
+
+case 'friday':{
+    console.log('FRIDAY');
+}
+break
+
+case 'saturday':{
+    console.log('SATURDAY');
+}
+break
+case 'sunday':{
+    console.log('SUNDAY');
+}
+break
+}
